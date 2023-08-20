57e8b220b6686629b347b7064560bc834c8742ad
profalakbear <>
2023-08-20 23:53:31 +0300

    added support for writing regarding package names for each go files.

--- main.go
+++ main.go
@@ -52,6 +52,33 @@ func initGoMod(basePath string) error {
 	return nil
 }
 
+func writePackageDeclaration(filePath string, packageName string) error {
+	// Read the content of the file
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Open the file for writing (truncating existing content)
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write package declaration and the original content
+	_, err = file.WriteString("package " + packageName + "\n")
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func readStructureFromFile(filename string) ([]string, error) {
 	content, err := os.ReadFile(filename)
 	if err != nil {
@@ -84,5 +111,35 @@ func main() {
 	// Initialize go.mod and go.sum
 	if err := initGoMod(*p); err != nil {
 		fmt.Println("Error initializing go.mod:", err)
+		return
+	}
+
+	// Write package declarations for .go files
+	err = filepath.Walk(*p, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(path, ".go") {
+			relPath, err := filepath.Rel(*p, path)
+			if err != nil {
+				return err
+			}
+
+			packageName := ""
+			if filepath.Base(path) == "main.go" {
+				packageName = "main"
+			} else {
+				dir := filepath.Dir(relPath)
+				packageName = filepath.Base(dir)
+			}
+
+			if err := writePackageDeclaration(path, packageName); err != nil {
+				fmt.Printf("Error writing package declaration for %s: %v\n", path, err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println("Error writing package declarations:", err)
 	}
 }
